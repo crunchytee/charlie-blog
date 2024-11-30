43376e0d25975acb587f2cb8815e7afc9355f6c6
crunchytee <tosh.robertsbrockway@gmail.com>
2024-11-30 13:33:36 -0800

    update password settings

--- app/models.py
+++ app/models.py
@@ -7,7 +7,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(128), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
+    password_hash = db.Column(db.String(512))
     posts = db.relationship("Post", backref="author", lazy="dynamic")
 
     def __repr__(self):
